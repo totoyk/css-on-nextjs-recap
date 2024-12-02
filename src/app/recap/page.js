468af4db2ee6f4deb105647c260daf71dca64228
me <you@example.com>
2024-12-02 15:16:47 +0900

    chap1

--- src/app/recap/page.js
+++ src/app/recap/page.js
@@ -1,10 +1,39 @@
 import Image from 'next/image'
-import sozaiPic from '@public/sozai.jpg'
+import sozaiPic from '@public/sozai.webp'
 
 const Recap = () => {
   return (
     <div className={`RecapContainer`}>
-      <Image src={sozaiPic} alt='sozai' />
+      <div className={`RecapPic`}>
+        <Image src={sozaiPic} alt='sozai' />
+      </div>
+      <div className={`RecapPic2`}>
+        <Image src={sozaiPic} alt='sozai' />
+      </div>
+      <div className={`RecapPic3`}>
+        <Image src={sozaiPic} alt='sozai' />
+      </div>
+      <div className={`RecapPic4`}>
+        <Image src={sozaiPic} alt='sozai' />
+      </div>
+      <div className={`RecapPic5`}>
+        <Image src={sozaiPic} alt='sozai' />
+      </div>
+      <div className={`RecapPic6`}>
+        <Image src={sozaiPic} alt='sozai' />
+      </div>
+      <div className={`RecapPic6_a`}>
+        <Image src={sozaiPic} alt='sozai' />
+      </div>
+      <div className={`RecapPic6_b`}>
+        <Image src={sozaiPic} alt='sozai' />
+      </div>
+      <div className={`RecapPic6_c`}>
+        <Image src={sozaiPic} alt='sozai' />
+      </div>
+      <div className={`RecapPic7`}>
+        <Image src={sozaiPic} alt='sozai' />
+      </div>
     </div>
   )
 }
